90db5735a87f8d6d63b5a349c7e6b7601a0808e1
JefflolC <jeffreychenubc@gmail.com>
2020-05-19 13:59:42 -0700

    Add UI for register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import RegisterForm from './components/registerForm';
+
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello, React!</h1>
+        <RegisterForm></RegisterForm>
       </div>
     )
   }
---
+++ src/components/registerForm.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './registerForm.css';
+
+export default class RegisterForm extends Component {
+  render() {
+    return (
+      <form className="register-form">
+        <div className="form-header">
+          <h1 className="form-title">Register</h1>
+          <h3 className="form-caption">Ready to add a new word?</h3>
+        </div>
+        <div className="username-input">
+          <label for="username">USERNAME</label>
+          <input type="text" id="username" name="username"></input>
+        </div>
+        <div className="password-input">
+          <label for="password">PASSWORD</label>
+          <input type="text" id="password" name="password"></input>
+        </div>
+        <button className="form-submit-btn">Register</button>
+        <div className="existing-account">
+          <span>Already have an account?</span>
+          <button className="sign-in-btn">Sign in!</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+/* TODO 
+ * Align [sign-in-btn] with rest of text
+ */
