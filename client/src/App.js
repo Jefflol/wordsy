aee5316c75d1b4228a35e617afd191fb21366f79
JefflolC <jeffreychenubc@gmail.com>
2020-05-19 19:53:32 -0700

    Setup dependencies and redux states

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-
 import RegisterForm from './components/registerForm';
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <RegisterForm></RegisterForm>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <RegisterForm />
+        </div>
+      </Provider>
     )
   }
 }
