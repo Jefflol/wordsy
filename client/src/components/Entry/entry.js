9497f89528b0188756fb05ff0daee2e611c661e1
JefflolC <jeffreychenubc@gmail.com>
2020-05-21 18:51:32 -0700

    Setup UI for entry component

---
+++ client/src/components/Entry/entry.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import './entry.css';
+
+export default class Entry extends Component {
+  render() {
+    const word = this.props.word;
+    const definition = this.props.definition[0].text;
+    const pos = this.props.pos;
+
+    return (
+      <div className="entry">
+        <div className="entry-word">
+          <EntryWord>{word}</EntryWord>
+        </div>
+        <div className="entry-definition">
+          <EntryDefinition>{definition}</EntryDefinition>
+        </div>
+        <div className="entry-parts-of-speech">
+          { pos.map(pos => <EntryPOS type={pos.text} />) }
+        </div>
+      </div>
+    );
+  }
+}
+
+class EntryWord extends Component {
+  render() {
+    return (
+      <div className="entry-word-bubble">
+        <div className="entry-word-text">
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
+
+class EntryDefinition extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+class EntryPOS extends Component {
+  getType = (type) => {
+    let posData = {};
+    
+    switch(type) {
+      case 'Noun':
+        posData.color = 'pos-blue';
+        posData.text = 'N';
+        break;
+      case 'Pronoun':
+        posData.color = 'pos-green';
+        posData.text = 'PN';
+        break;
+      case 'Adjective':
+        posData.color = 'pos-yellow';
+        posData.text = 'Adj';
+        break;
+      case 'Verb':
+        posData.color = 'pos-orange';
+        posData.text = 'V';
+        break;
+      case 'Adverb':
+        posData.color = 'pos-red';
+        posData.text = 'Adv';
+        break;
+      case 'Preposition':
+        posData.color = 'pos-dark-red';
+        posData.text = 'Pre';
+        break;
+      case 'Other':
+      default:
+        posData.color = 'pos-grey';
+        posData.text = 'O';
+        break;
+    }
+
+    return posData;
+  }
+
+  render() {
+    const typeData = this.getType(this.props.type);
+    const classNames = `entry-parts-of-speech-icon ${typeData.color}`;
+
+    return (
+      <div className={classNames}>
+        {typeData.text}
+      </div>
+    );
+  }
+}
--- client/src/components/wordBank.js
+++ client/src/components/wordBank.js
@@ -2,13 +2,36 @@ import React, { Component } from 'react';
 import './wordBank.css';
 import { connect } from 'react-redux';
 
+import Entry from './Entry/entry';
+
 class WordBank extends Component {
+  state = {
+    word: 'Baleful',
+    definition: [{
+      id: 1,
+      text: 'Threatening harm; menancing'
+    }],
+    pos: [{
+      id: 1,
+      text: 'Adjective'
+    },
+    {
+      id: 2,
+      text: 'Noun'
+    }]
+  }
+
   render() {
     return (
       <div className="word-bank-container">
         {
           // this.props.isLoggedIn &&
           <div className="word-bank">
+            <Entry 
+              word={this.state.word}
+              definition={this.state.definition}
+              pos={this.state.pos}
+            />
           </div>
         }
       </div>
