7c840f13969da96d473c409f5db493168277cd26
JefflolC <jeffreychenubc@gmail.com>
2020-06-12 20:58:04 -0700

    Refactor WordDetails component

--- client/src/components/WordDetails/wordDetails.js
+++ client/src/components/WordDetails/wordDetails.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import { ReactComponent as EditIcon } from '../../assets/edit.svg';
 import { ReactComponent as CloseIcon } from '../../assets/x-circle.svg';
+import { getPartsOfSpeechData } from '../partsOfSpeech';
 
 import './wordDetails.css';
 
@@ -10,41 +11,63 @@ export default class WordDetails extends Component {
     return (
       <div className="word-details-container">
         <div className="word-details">
-          <div className="word-header">
-            <div className="word-bubble">
-              <div className="word-text">
-                Baleful
-              </div>
-            </div>
-            <button className="edit-btn">
-              <EditIcon />
-            </button>
-            <button className="close-btn">
-              <CloseIcon />
-            </button>
+          <div className="word-details-header">
+            <Word text="Baleful" />
+            <button className="edit-btn"><EditIcon /></button>
+            <button className="close-btn"><CloseIcon /></button>
           </div>
-          <div className="word-definitions">
-            <div className="word-definitions-title">DEFINITION</div>
-            <div className="word-definitions-entry">
-              <div className="word-definitions-lexeme lexeme-background-blue lexeme-text-blue">
-                N
-                <div className="word-definitions-lexeme-number">1</div>
-              </div>
-              <div className="word-definitions-definition">Threatening harm; menacing</div>
+          <div className="word-details-body">
+            <div className="body-title">DEFINITION</div>
+            <div className="body-entry">
+              <WordLexeme type="Noun" order="1"/>
+              <WordDefinition definition="Threatening harm; menacing" />
             </div>
           </div>
-          <div className="word-examples">
-            <div className="word-examples-title">EXAMPLE</div>
-            <div className="word-examples-entry">
-              <div className="word-examples-lexeme lexeme-background-blue lexeme-text-blue">
-                N
-                <div className="word-definitions-lexeme-number">1</div>
-              </div>
-              <div className="word-examples-example">What a <i>baleful</i> personality!</div>
+          <div className="word-details-body">
+            <div className="body-title">EXAMPLE</div>
+            <div className="body-entry">
+              <WordLexeme type="Noun" order="1"/>
+              <WordExample example="What a baleful personality!" />
             </div>
           </div>
         </div>
       </div>
     );
   }
+}
+
+const Word = props => {
+  return (
+    <div className="word-container">
+      <div className="word-text">
+        {props.text}
+      </div>
+    </div>
+  );
+}
+
+const WordLexeme = props => {
+  let { color: lexemeColor, text: lexemeText } = getPartsOfSpeechData(props.type);
+  let classNames = `word-lexeme lexeme-background-${lexemeColor} lexeme-text-${lexemeColor}`;
+
+  return (
+    <>
+      <div className={classNames}>
+        {lexemeText}
+        <div className="word-lexeme-order">{props.order}</div>
+      </div>
+    </>
+  );
+}
+
+const WordDefinition = props => {
+  return (
+    <div className="word-definition">{props.definition}</div>
+  );
+}
+
+const WordExample = props => {
+  return (
+    <div className="word-example">{props.example}</div>
+  );
 }
