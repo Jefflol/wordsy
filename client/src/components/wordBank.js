0281de923a341f6b517e5799deec48b7ef23f6c4
JefflolC <jeffreychenubc@gmail.com>
2020-06-12 23:09:08 -0700

    Add dropdown menu to options

--- client/src/components/wordBank.js
+++ client/src/components/wordBank.js
@@ -5,12 +5,16 @@ import Entry from './Entry/entry';
 import { getWordEntries } from '../actions/entryActions';
 import { ReactComponent as MoreIcon } from '../assets/more-horizontal.svg';
 
+import DropDown, { DropDownOption } from '../components/dropDown';
+
 import './wordBank.css';
 
 
 class WordBank extends Component {
   state = {
     entry: [],
+    showOptions: false,
+    showDefinitions: true
   }
 
   componentDidMount() {
@@ -27,7 +31,18 @@ class WordBank extends Component {
   }
 
   toggleOptions = () => {
-    console.log('Toggled');
+    this.setState(prevState => ({
+      showOptions: !prevState.showOptions
+    }));
+  }
+
+  toggleDefinitions = () => {
+    console.log('Toggle definitions');
+
+    this.setState(prevState => ({
+      showOptions: false,
+      showDefinitions: !prevState.showDefinitions
+    }));
   }
 
   renderWordBank = () => {
@@ -60,7 +75,21 @@ class WordBank extends Component {
       <div className="word-bank-container">
         <div className="word-bank-header">
           <div className="header-title">Your Words</div>
-          <MoreIcon className="word-bank-options" onClick={this.toggleOptions} />
+            <MoreIcon className="word-bank-options" onClick={this.toggleOptions} />           
+            { 
+              this.state.showOptions && 
+              <div className="options-dropdown">
+                <DropDown>
+                  { 
+                    this.state.showDefinitions ?
+                    <DropDownOption text="Hide definition" onClick={this.toggleDefinitions}/> :
+                    <DropDownOption text="Show definition" onClick={this.toggleDefinitions}/>
+                  }
+                  <DropDownOption text="Sort by recent" onClick={this.toggleDefinitions}/>
+                  <DropDownOption text="Sort by alphabetical" onClick={this.toggleDefinitions}/>
+                </DropDown> 
+              </div>
+            } 
         </div>
         <div className="word-bank">
           { wordBank }
