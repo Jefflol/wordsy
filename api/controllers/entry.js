a15ccf3a8925e3a64293f037e4704af6a2c7ef20
JefflolC <jeffreychenubc@gmail.com>
2020-06-17 22:19:03 -0700

    Move entry functionalities to a controller file

---
+++ api/controllers/entry.js
@@ -0,0 +1,168 @@
+const mongoose = require('mongoose');
+
+const { Entry, Definition, Example } = require('../models/entry-model');
+
+
+exports.entry_get_all = (req, res, next) => {
+  // Check if user is authorized
+  if (req.params.userId !== req.userData.userId) {
+    return res.status(401).json({
+      message: 'Unauthorized'
+    });
+  }
+
+  // Sort entries alphabetically or recently (default)
+  const sortQuery = (req.query.sort === 'alphabet') ? { word: 1 } : { dateCreated: 1 };
+
+  Entry.find({ userId: req.params.userId })
+    .select('_id userId word definition example dateCreated')
+    .sort(sortQuery)
+    .collation({ locale: 'en' })
+    .then(entries => {
+      console.log('Entries found ', entries);
+      return res.status(200).json({
+        entry: entries
+      });
+    })
+    .catch(err => {
+      return res.status(500).json({
+        error: err
+      });
+    });
+};
+
+
+exports.entry_get_one = (req, res, next) => {
+  // Check if user is authorized
+  if (req.params.userId !== req.userData.userId) {
+    return res.status(401).json({
+      message: 'Unauthorized'
+    });
+  }
+
+  Entry.findOne({ userId: req.params.userId, _id: req.params.entryId })
+    .select('_id userId word definition example dateCreated')
+    .then(entry => {
+      console.log('Entry found ', entry);
+
+      if (entry) {
+        return res.status(200).json({
+          entry: entry
+        });
+      } else {
+        return res.status(404).json({
+          message: 'No valid entry found for provided ID'
+        });
+      }
+    })
+    .catch(err => {
+      return res.status(500).json({
+        error: err
+      });
+    });
+};
+
+
+exports.entry_add = (req, res, next) => {
+  // Check if user is authorized
+  if (req.params.userId !== req.userData.userId) {
+    return res.status(401).json({
+      message: 'Unauthorized'
+    });
+  }
+
+  const id = new mongoose.Types.ObjectId();
+  const definitions = req.body.definition.map(definition => {
+    return new Definition({
+      _id: id,
+      partsOfSpeech: definition.partsOfSpeech,
+      definition: definition.definition
+    });
+  });
+
+  const examples = req.body.example.map(example => {
+    return new Example({
+      _id: id,
+      partsOfSpeech: example.partsOfSpeech,
+      example: example.example
+    });
+  });
+
+  const entry = new Entry({
+    _id: new mongoose.Types.ObjectId(),
+    userId: req.body.userId,
+    word: req.body.word,
+    definition: [...definitions],
+    example: [...examples]
+  });
+
+  entry.save()
+    .then(result => {
+      console.log(result);
+      return res.status(201).json({
+        message: 'Entry created'
+      });
+    })
+    .catch(err => {
+      return res.status(500).json({
+        error: err
+      });
+    });
+};
+
+
+exports.entry_delete = (req, res, next) => {
+  // Check if user is authorized
+  if (req.params.userId !== req.userData.userId) {
+    return res.status(401).json({
+      message: 'Unauthorized'
+    });
+  }
+
+  Entry.deleteOne({ _id: req.params.entryId })
+    .then(entry => {
+      if (entry.deletedCount) {
+        return res.status(200).json({
+          message: `Entry [${req.params.entryId}] deleted`
+        });
+      } else {
+        return res.status(404).json({
+          message: `Entry [${req.params.entryId}] not found`
+        });
+      }
+    })
+    .catch(err => {
+      return res.status(500).json({
+        error: err
+      });
+    });
+};
+
+
+exports.entry_edit = (req, res, next) => {
+  // Check if user is authorized
+  if (req.params.userId !== req.userData.userId) {
+    return res.status(401).json({
+      message: 'Unauthorized'
+    });
+  }
+
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+
+  Entry.update({ _id: req.params.entryId }, { $set: updateOps })
+  .then(result => {
+    return res.status(200).json({
+      entry: result,
+      message: 'Entry updated',
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    return res.status(500).json({
+      error: err
+    });
+  });
+};
--- api/routes/entry.js
+++ api/routes/entry.js
@@ -1,209 +1,34 @@
 const express = require('express');
 const router = express.Router();
 
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-
-const { Entry, Definition, Example } = require('../models/entry-model');
+const EntryController = require('../controllers/entry');
 const checkAuth = require('../middleware/check-auth');
 
-// @route   GET /entry
-// @desc    Fetch all entries
-// @access  PRIVATE
-// router.get('/', (req, res, next) => {
-//   Entry.find()
-//     .select('_id userId word definition example dateCreated')
-//     .then(entries => {
-//       console.log('Entries found ', entries);
-//       return res.status(200).json({
-//           entry: entries
-//       });
-//     })
-//     .catch(err => {
-//       return res.status(500).json({
-//         error: err
-//       });
-//     });
-// });
 
-// @route   GET /entry/:userId
+// @route   GET /entry/:userId?sort=
 // @desc    Fetch all entries for a specific user
 // @access  PRIVATE
-router.get('/:userId', checkAuth, (req, res, next) => {
-  // Check if user is authorized
-  if (req.params.userId !== req.userData.userId) {
-    return res.status(401).json({
-      message: 'Unauthorized'
-    });
-  }
-
-  // Sort entries alphabetically or recently (default)
-  const sortQuery = (req.query.sort === 'alphabet') ? { word: 1 } : { dateCreated: 1 };
-
-  Entry.find({ userId: req.params.userId })
-    .select('_id userId word definition example dateCreated')
-    .sort(sortQuery)
-    .collation({ locale: 'en' })
-    .then(entries => {
-      console.log('Entries found ', entries);
-      return res.status(200).json({
-        entry: entries
-      });
-    })
-    .catch(err => {
-      return res.status(500).json({
-        error: err
-      });
-    });
-});
-
+router.get('/:userId', checkAuth, EntryController.entry_get_all);
 
 // @route   GET /entry/:userId/:entryId
 // @desc    Fetch an entry by ID
 // @access  PRIVATE
-router.get('/:userId/:entryId', checkAuth, (req, res, next) => {
-  // Check if user is authorized
-  if (req.params.userId !== req.userData.userId) {
-    return res.status(401).json({
-      message: 'Unauthorized'
-    });
-  }
-
-  Entry.findOne({ userId: req.params.userId, _id: req.params.entryId })
-    .select('_id userId word definition example dateCreated')
-    .then(entry => {
-      console.log('Entry found ', entry);
-
-      if (entry) {
-        return res.status(200).json({
-          entry: entry
-        });
-      } else {
-        return res.status(404).json({
-          message: 'No valid entry found for provided ID'
-        });
-      }
-    })
-    .catch(err => {
-      return res.status(500).json({
-        error: err
-      });
-    });
-});
-
+router.get('/:userId/:entryId', checkAuth, EntryController.entry_get_one);
 
 // @route   POST /entry/:userId
 // @desc    Create an entry
 // @access  PRIVATE
-router.post('/:userId', checkAuth, (req, res, next) => {
-  // Check if user is authorized
-  if (req.params.userId !== req.userData.userId) {
-    return res.status(401).json({
-      message: 'Unauthorized'
-    });
-  }
-
-  const id = new mongoose.Types.ObjectId();
-  const definitions = req.body.definition.map(definition => {
-    return new Definition({
-      _id: id,
-      partsOfSpeech: definition.partsOfSpeech,
-      definition: definition.definition
-    });
-  });
-
-  const examples = req.body.example.map(example => {
-    return new Example({
-      _id: id,
-      partsOfSpeech: example.partsOfSpeech,
-      example: example.example
-    });
-  });
-
-  const entry = new Entry({
-    _id: new mongoose.Types.ObjectId(),
-    userId: req.body.userId,
-    word: req.body.word,
-    definition: [...definitions],
-    example: [...examples]
-  });
-
-  entry.save()
-    .then(result => {
-      console.log(result);
-      return res.status(201).json({
-        message: 'Entry created'
-      });
-    })
-    .catch(err => {
-      return res.status(500).json({
-        error: err
-      });
-    });
-});
-
+router.post('/:userId', checkAuth, EntryController.entry_add);
 
 // @route   DELETE /entry/:userId/:entryId
 // @desc    Remove an entry
 // @access  PRIVATE
-router.delete('/:userId/:entryId', checkAuth, (req, res, next) => {
-  // Check if user is authorized
-  if (req.params.userId !== req.userData.userId) {
-    return res.status(401).json({
-      message: 'Unauthorized'
-    });
-  }
-
-  Entry.deleteOne({ _id: req.params.entryId })
-    .then(entry => {
-      if (entry.deletedCount) {
-        return res.status(200).json({
-          message: `Entry [${req.params.entryId}] deleted`
-        });
-      } else {
-        return res.status(404).json({
-          message: `Entry [${req.params.entryId}] not found`
-        });
-      }
-    })
-    .catch(err => {
-      return res.status(500).json({
-        error: err
-      });
-    });
-});
-
+router.delete('/:userId/:entryId', checkAuth, EntryController.entry_delete);
 
 // @route   PATCH /entry/:entryId
 // @desc    Edit an entry
 // @access  PRIVATE
-router.patch('/:entryId', checkAuth, (req, res, next) => {
-  // Check if user is authorized
-  if (req.params.userId !== req.userData.userId) {
-    return res.status(401).json({
-      message: 'Unauthorized'
-    });
-  }
+router.patch('/:entryId', checkAuth, EntryController.entry_edit);
 
-  const updateOps = {};
-  for (const ops of req.body) {
-    updateOps[ops.propName] = ops.value;
-  }
-
-  Entry.update({ _id: req.params.entryId }, { $set: updateOps })
-  .then(result => {
-    return res.status(200).json({
-      entry: result,
-      message: 'Entry updated',
-    });
-  })
-  .catch(err => {
-    console.log(err);
-    return res.status(500).json({
-      error: err
-    });
-  });
-});
 
 module.exports = router;
