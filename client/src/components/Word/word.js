30fcaf15904cdc3163ca21bc91c9fff7604144ae
JefflolC <jeffreychenubc@gmail.com>
2020-06-20 14:44:01 -0700

    Separate WordEntry to its own file

--- client/src/components/Word/word.js
+++ client/src/components/Word/word.js
@@ -1,65 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import { deleteWordEntry, getWordEntry } from '../../actions/entryActions';
 import { getPartsOfSpeechData } from '../partsOfSpeech';
-import { connect } from 'react-redux';
 
-import './entry.css';
-
-
-class WordEntry extends Component {
-  // Delete word entry
-  handleDeleteEntry = (e, wordId) => {
-    e.stopPropagation();
-
-    this.props.deleteWordEntry(this.props.userId, wordId);
-  }
-
-  // Show word details
-  handleClickEntry = (e, wordId) => {
-    e.stopPropagation();
-
-    this.props.getWordEntry(this.props.userId, wordId);
-  }
-
-  render() {
-    const { word, definition, lexemes, id, show } = this.props;
-
-    return (
-      <div className="entry">
-        <div className="entry-word">
-          <Word
-            text={word}
-            onClick={e => this.handleClickEntry(e, id)} 
-            onDelete={e => this.handleDeleteEntry(e, id)} 
-          />
-        </div>
-        <div className="entry-definition">
-          { 
-            show && 
-            <WordDefinition text={definition} /> 
-          }
-        </div>
-        <div className="entry-lexeme">
-          { 
-            lexemes.map(lexeme => 
-              <WordLexeme key={`${id}+${lexeme}`} type={lexeme} />
-            ) 
-          }
-        </div>
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = state => ({
-  userId: state.user.userId
-});
-
-export default connect(mapStateToProps, {
-  deleteWordEntry,
-  getWordEntry
-})(WordEntry);
+import './word.css';
 
 
 export const Word = props => {
---
+++ client/src/components/Word/wordEntry.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+
+import { Word, WordDefinition, WordLexeme } from './word';
+import { deleteWordEntry, getWordEntry } from '../../actions/entryActions';
+import { connect } from 'react-redux';
+
+import './wordEntry.css';
+
+
+class WordEntry extends Component {
+  // Delete word entry
+  handleDeleteEntry = (e, wordId) => {
+    e.stopPropagation();
+
+    this.props.deleteWordEntry(this.props.userId, wordId);
+  }
+
+  // Show word details
+  handleClickEntry = (e, wordId) => {
+    e.stopPropagation();
+
+    this.props.getWordEntry(this.props.userId, wordId);
+  }
+
+  render() {
+    const { word, definition, lexemes, id, show } = this.props;
+
+    return (
+      <div className="entry">
+        <div className="entry-word">
+          <Word
+            text={word}
+            onClick={e => this.handleClickEntry(e, id)} 
+            onDelete={e => this.handleDeleteEntry(e, id)} 
+          />
+        </div>
+        <div className="entry-definition">
+          { 
+            show && 
+            <WordDefinition text={definition} /> 
+          }
+        </div>
+        <div className="entry-lexeme">
+          { 
+            lexemes.map(lexeme => 
+              <WordLexeme key={`${id}+${lexeme}`} type={lexeme} />
+            ) 
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  userId: state.user.userId
+});
+
+export default connect(mapStateToProps, {
+  deleteWordEntry,
+  getWordEntry
+})(WordEntry);
