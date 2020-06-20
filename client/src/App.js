09cf25838f07a6ebd7eb2a4baa3f4a02b4e431a7
JefflolC <jeffreychenubc@gmail.com>
2020-06-20 15:02:36 -0700

    Refactor App

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,14 @@
 import React, { Component } from 'react';
-// import './App.css';
+
+import AppRootComponent from './components/AppRootComponent';
 import { Provider } from 'react-redux';
 import store from './store';
 
-// import UserForm from './components/UserForm/userForm';
-// import WordBank from './components/wordBank';
-// import EntryForm from './components/EntryForm/entryForm';
-
-import AppRootComponent from './components/AppRootComponent';
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        {/* <div className="App">
-          <EntryForm />
-          <WordBank />
-          <UserForm />
-        </div> */}
         <AppRootComponent />
       </Provider>
     )
